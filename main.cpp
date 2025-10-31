852d5649d6aea0bab86450fd12c2597b9b79497e
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-31 15:08:08 +0800

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+unsigned int windowWidth = 800;
+unsigned int windowHeight = 800;
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Gravity Sim", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to load window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
